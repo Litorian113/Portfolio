20b8203c5d3f69cad69b53915588f46773d0ffe3
Litorian113 <154338974+Litorian113@users.noreply.github.com>
2025-03-05 12:53:44 +0100

    Füge neue Seiten für die Projekte Bwegt und NASS hinzu und implementiere Navigation zurück zur Hauptseite mit Parametern

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <!-- src/routes/+page.svelte -->
-<script>
+<script context="module">
+    export async function load({ url }) {
+      return {
+        cx: Number(url.searchParams.get('cx')) || 0,
+      cy: Number(url.searchParams.get('cy')) || 0,
+      cz: Number(url.searchParams.get('cz')) || 20
+      };
+    }
+    export let cx;
+    export let cy;
+    export let cz;
     import Stage from '$lib/Stage.svelte';
-	import Test from '$lib/Test.svelte';
   </script>
-
-
-  <!-- <Test /> -->
-  <Stage />
+  
+  <Stage {cx} {cy} {cz} />
   
---
+++ src/routes/project/nass/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { page } from '$app/stores';
+    import { goto } from '$app/navigation';
+  
+    $: url = $page.url;
+  
+    function goBackToFlur() {
+      const cx = url.searchParams.get('cx') ?? 0;
+      const cy = url.searchParams.get('cy') ?? 0;
+      const cz = url.searchParams.get('cz') ?? 20;
+      goto(`/?cx=${cx}&cy=${cy}&cz=${cz}`);
+    }
+  </script>
+  
+  <h1>Projekt: NASS</h1>
+  <p>Hier steht dein Content zum Projekt Nass.</p>
+  <button on:click={goBackToFlur}>Zurück zum Flur</button>
+  
